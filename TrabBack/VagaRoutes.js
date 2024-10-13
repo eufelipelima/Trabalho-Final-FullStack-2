3d01867bc4758f2aeee461e10eefe34399522049
eufelipelima <157855349+eufelipelima@users.noreply.github.com>
2024-10-13 19:47:19 -0300

    Delete TrabBack/VagaRoutes.js

--- TrabBack/VagaRoutes.js
+++
@@ -1,8 +0,0 @@
-const VagaController = require('../controller/VagaController');
-
-const vagasController = new VagaController();
-const express = require('express');
-const router =express.Router();
-router.get('/vagas',vagasController.obterTodos)
-
-module.exports=router;
