4ba8f02d7872815e53ed1630c6abdbbce3ba5094
eufelipelima <157855349+eufelipelima@users.noreply.github.com>
2024-10-13 19:46:02 -0300

    Delete TrabBack/CandidatoRoutes.js

--- TrabBack/CandidatoRoutes.js
+++
@@ -1,8 +0,0 @@
-const CandidatoController = require('../controller/CandidatoController');
-
-const candidatosController = new CandidatoController();
-const express = require('express');
-const router =express.Router();
-router.get('/candidatos',candidatosController.obterTodos)
-
-module.exports=router;
